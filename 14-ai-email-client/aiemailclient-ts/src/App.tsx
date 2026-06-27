ce159131bd474f6a7c474af9d4bc023920ee1cd7
ismail <ismahm01@easv365.dk>
2026-06-27 02:32:21 +0200

    Changed folder names, so that when downloaded on windows, the folders dont merge. Since Windows treats folders/files with the same letters the same file, regardless of casing.

--- 14-ai-email-client/aiemailclient-ts/src/App.tsx
+++ 14-ai-email-client/aiemailclient-ts/src/App.tsx
@@ -12,7 +12,6 @@ function LoginView() {
     return (
         <div className="flex flex-col items-center justify-center w-150 h-100 bg-[#111622] border-2 border-[#1d263b] rounded-2xl">
             <h1 className="flex w-full justify-center items-center text-white font-bold text-2xl">Log In</h1>
-
         </div>
     );
 }
